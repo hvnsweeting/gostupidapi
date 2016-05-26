a9c0f62871e3743cd2a9a51bf3aa02049893beaa
Hung Nguyen Viet <hvn@familug.org>
2016-05-26 20:39:15 +0700

    add boltdb

--- server.go
+++ server.go
@@ -1,76 +1,133 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"net/http"
+	"log"
 
-	// log "github.com/Sirupsen/logrus"
+	"github.com/boltdb/bolt"
+	// db "github.com/hvnsweeting/meomeo/db"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/engine/standard"
-	"log"
+
+	"net/http"
 )
+
+var db bolt.DB
 
 func init() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 }
 
-func Index(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+type Entry struct {
+	IP      string
+	Comment string
 }
 
-func TaiLieuHocTap(c echo.Context) error {
-	return c.String(http.StatusForbidden, "Forbidden")
+type ReturnMessage struct {
+	Message string
 }
 
-func LoginHandler(c echo.Context) error {
-	return c.JSON(http.StatusOK, c.QueryParams())
-}
-
-type MyData struct {
-	Password []string
-	User     []string `json:"user_name"`
-}
-
-type ReturnData struct {
-	Password []string
-	User     []string
-}
-
-func JsonEndpoint(c echo.Context) error {
-	r := c.Request()
-
-	data, err := ioutil.ReadAll(r.Body())
-	log.Println(string(data))
-	var myData MyData
-	if err == nil && data != nil {
-		err = json.Unmarshal(data, &myData)
-		if err != nil {
-			log.Println("Marshalling: ", err)
-			return c.JSON(http.StatusBadRequest, "Bad JSON")
-		}
-	}
-
-	retData := ReturnData{User: myData.User, Password: myData.Password}
-	return c.JSON(http.StatusOK, retData)
-}
-
-func JsonIndustry(c echo.Context) error {
-	var myData MyData
-	err := c.Bind(myData)
+func AddIP(c echo.Context) error {
+	var e Entry
+	err := c.Bind(&e)
 	if err != nil {
-		log.Println(err)
+		log.Println("error", err)
 	}
-	return c.JSON(http.StatusOK, myData)
+
+	db, err := bolt.Open("my.db", 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// add entry to database
+	err = db.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucketIfNotExists([]byte("nambk"))
+		if err != nil {
+			return err
+		}
+		err = b.Put([]byte(e.Comment), []byte(e.IP))
+		return err
+	})
+
+	if err != nil {
+		log.Print("DB update failed", err)
+	}
+
+	msg := ReturnMessage{"success"}
+
+	log.Println(msg)
+
+	return c.JSON(http.StatusCreated, msg)
+}
+
+func CheckBlacklistHandler(c echo.Context) error {
+	ipCheck := c.QueryParam("ip")
+	log.Println("INPUT: ", ipCheck)
+	if len(ipCheck) == 0 {
+		c.JSON(http.StatusBadRequest, ReturnMessage{"failed"})
+	}
+
+	db, err := bolt.Open("my.db", 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// add entry to database
+	found := false
+	err = db.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucketIfNotExists([]byte("nambk"))
+		if err != nil {
+			return err
+		}
+		c := b.Cursor()
+
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			if string(v) == ipCheck {
+				found = true
+			}
+		}
+
+		return err
+	})
+
+	if err != nil {
+		log.Print("DB update failed", err)
+	}
+
+	var msg ReturnMessage
+	if found {
+		msg = ReturnMessage{"found"}
+	} else {
+		msg = ReturnMessage{"not found"}
+	}
+
+	log.Println(msg)
+
+	return c.JSON(http.StatusCreated, msg)
+
 }
 
 func main() {
+
+	//	db, err := bolt.Open("my.db", 0600, nil)
+	//	if err != nil {
+	//		log.Fatal(err)
+	//	}
+	//	defer db.Close()
+	//	log.Println(db.Info())
+
 	e := echo.New()
 	e.GET("/", Index)
+
+	e.POST("/blacklist/ip", AddIP)
+	e.Get("/blacklist/ip", CheckBlacklistHandler)
+
 	e.GET("/tlht", TaiLieuHocTap)
 	e.POST("/login", LoginHandler)
 	e.POST("/hang", JsonEndpoint)
 	e.POST("/ind", JsonIndustry)
 	e.SetDebug(true)
+
 	e.Run(standard.New(":1323"))
 }
